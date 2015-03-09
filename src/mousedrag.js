72189828ce6a9ed823461fe8f0bf98b035062fba
polo2ro <paul@rosanbo.com>
2015-03-09 23:13:15 +0100

    dragmove

--- src/mousedrag.js
+++ src/mousedrag.js
@@ -12,19 +12,69 @@ function MouseDrag(teleperiod) {
     var xStart = d3.mouse(teleperiod.viewport.node())[0];
     var currentX = parseInt(teleperiod.main.attr('x'), 10);
     var currentWidth = parseInt(teleperiod.main.attr("width"), 10);
+    var viewportWidth = parseInt(teleperiod.viewport.attr('width'), 10);
 
+     var viewportX = teleperiod.viewport.attr('x');
+
+    if (null === viewportX) {
+        viewportX = 0;
+    } else {
+        viewportX = parseInt(viewportX, 10);
+    }
 
     // additional width added when backwardGrow() or forwardGrow()
     var additionalWidth = (teleperiod.getMoveDays() * teleperiod.getDateWidth());
 
     var mouseDrag = this;
 
-    var x,
-        newX,
-        gapDaysBackward,
-        hiddenPart,
-        gapDaysForward,
-        backwardTotal = 0;
+    var x, newX;
+
+
+    /**
+     * Get the distance from the main sliding frame to the fixed viewport
+     * positive if the main frame left x position is lower than the viewport left x position
+     * @return {Int}
+     */
+    this.getLeftDistance = function() {
+
+        var main = teleperiod.main.attr('x');
+
+        if (null === main) {
+            main = 0;
+        } else {
+            main = parseInt(main, 10);
+        }
+
+        return (viewportX - main);
+    };
+
+
+    /**
+     * Get the distance from the main sliding frame to the fixed viewport
+     * positive if the main frame right x position is higher than the viewport right x position
+     * @return {Int}
+     */
+    this.getRightDistance = function() {
+
+        var viewport = viewportX + viewportWidth;
+
+        var main = teleperiod.main.attr('x');
+        if (null === main) {
+            main = 0;
+        } else {
+            main = parseInt(main, 10);
+        }
+
+
+        var realCurrentWidth = parseInt(teleperiod.main.attr('width'), 10);
+
+        main += realCurrentWidth;
+
+        console.log('viewport '+viewport+' x '+teleperiod.main.attr('x')+' realCurrentWidth '+realCurrentWidth);
+
+        return (main - viewport);
+    };
+
 
 
     this.dragmove = function dragMove() {
@@ -33,34 +83,24 @@ function MouseDrag(teleperiod) {
         // relative x from the mouse origin
         newX = x - xStart;
 
-        // numbers of days between main x and viewport x
-        gapDaysBackward = Math.ceil((-1 * (newX + currentX))/teleperiod.getMoveDays());
-
-
-
-        hiddenPart = (-1*newX + Math.abs(currentX) + backwardTotal);
-
-        // number of days between main end and viewport end
-        gapDaysForward = Math.ceil((currentWidth - hiddenPart - teleperiod.getWidth())/teleperiod.getDateWidth()) -1;
-
-        teleperiod.main.attr('x', function() {
-
-            if (gapDaysBackward < 0) {
-                // before viewportFrom with at least one day
-                teleperiod.backwardGrow();
+        if (mouseDrag.getLeftDistance() < 0) {
+            // before viewportFrom with at least one day
+            if (teleperiod.backwardGrow()) {
                 currentX -= additionalWidth;
-                backwardTotal -= additionalWidth;
-                console.log('backwardGrow '+gapDaysBackward);
+                console.log('---');
             }
+        }
 
 
-            if (gapDaysForward < 0) {
-                teleperiod.forwardGrow();
+        if (mouseDrag.getRightDistance() < 0) {
+            if (teleperiod.forwardGrow()) {
                 currentWidth += additionalWidth;
-                console.log('forwardGrow '+gapDaysForward);
+                console.log('+++');
+                teleperiod.forwardGrow();
             }
+        }
 
-            return (currentX + newX);
-        });
+
+        teleperiod.main.attr('x', (currentX + newX));
     };
 }
