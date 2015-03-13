6f4e4eb272bfc555b8383360c5be964858722800
polo2ro <paul@rosanbo.com>
2015-03-13 23:30:45 +0100

    fix prefix in css

--- src/teleperiod.js
+++ src/teleperiod.js
@@ -314,6 +314,7 @@ function Teleperiod(settings) {
      */
     this.draw = function()
     {
+        telep.viewport.classed('teleperiod', true);
         telep.setSize();
         telep.createMain();
     };
