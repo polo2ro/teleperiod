178b10ae6fe8667e7648efa0d880c7b7ab7522e1
polo2ro <paul@rosanbo.com>
2019-12-01 10:10:45 +0100

    Add tests skeleton

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,13 +28,23 @@ module.exports = function (grunt) {
                     'dist/teleperiod.min.css': ['styles/teleperiod.css']
                 }
             }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['tests/**/*.js']
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-mocha-test');
 
+    grunt.registerTask('test', ['jshint', 'mochaTest']);
     grunt.registerTask('default', [
         'uglify', 'cssmin'
     ]);
---
+++ tests/teleperiodTest.js
@@ -0,0 +1,17 @@
+/*global describe: false, it: false */
+'use strict';
+
+var assert = require('assert');
+var jurassic = require('../src/teleperiod');
+
+describe('Teleperiod', function() {
+    describe('getWorkingTimesFromEvent()', function() {
+
+
+        it('test', function() {
+
+
+            assert.equal(3, 1);
+        });
+    });
+});
